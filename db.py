ce9ec1754b326b4d65caf7325b59458a1bdf07a9
Carlos Ng <carlosngva@outlook.com>
2022-08-14 23:52:01 -0600

    Ajustes en base de datos

--- db.py
+++ db.py
@@ -3,15 +3,11 @@ import pyodbc
 host = 'localhost'
 bd_name = 'ss2_practica1'
 username = 'sa'
-password = '7Dejunio'
+password = 'TEST'
 
 def connect_db():
     try:
-        connection = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=' +
-                                host + ';DATABASE=' + bd_name + ';UID=' + username + ';TrustServerCertificate=yes;PWD=' + password)
-
+        connection = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=' + host + ';DATABASE=' + bd_name + ';UID=' + username + ';TrustServerCertificate=yes;PWD=' + password)
         return connection
-        # OK! conexión exitosa
     except Exception as e:
-        # Atrapar error
-        print("Ocurrió un error al conectar a SQL Server: ", e)
+        print("An error has occured: ", e)
